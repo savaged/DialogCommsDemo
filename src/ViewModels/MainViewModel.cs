b7cc3e12e61f28990a8954e0d62e074145737135
David Savage <david@savaged.info>
2019-02-22 14:27:12 +0000

    Finishing touches to the demo

--- src/ViewModels/MainViewModel.cs
+++ src/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using DialogCommsDemo.Interfaces;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using MvvmDialogs;
+using System;
 
 namespace DialogCommsDemo.ViewModels
 {
@@ -23,6 +24,8 @@ namespace DialogCommsDemo.ViewModels
 
             ShowAnotherDialogCmd = new RelayCommand(OnShowAnotherDialog);
 
+            ShowModalDialogCmd = new RelayCommand(OnShowModalDialog);
+
             _dialogViewModel.DialogClosed += OnDialogClosed;
         }
 
@@ -30,12 +33,21 @@ namespace DialogCommsDemo.ViewModels
 
         public RelayCommand ShowAnotherDialogCmd { get; }
 
+        public RelayCommand ShowModalDialogCmd { get; }
+
         public string DialogOutput
         {
             get => _dialogOutput;
             set => Set(ref _dialogOutput, value);
         }
 
+        /// <summary>
+        /// The dialog result is captured in the event 
+        /// wired up in the ctor above like this:
+        /// _dialogViewModel.DialogClosed += OnDialogClosed;
+        /// which can do something like:
+        /// bool? result = e.DialogResult;
+        /// </summary>
         private void OnShowDialog()
         {
             _dialogService.Show(this, _dialogViewModel);
@@ -44,6 +56,20 @@ namespace DialogCommsDemo.ViewModels
         private void OnShowAnotherDialog()
         {
             _dialogService.Show(this, new AnotherDialogViewModel(this));
+        }
+
+        /// <summary>
+        /// Modal dialogs still work as expected
+        /// </summary>
+        private void OnShowModalDialog()
+        {
+            var result = _dialogService.ShowDialog(this, _dialogViewModel);
+            if (result is null)
+            {
+                throw new InvalidOperationException(
+                    "Expected the dialog to always return a valid boolean value!");
+            }
+            DialogOutput = result.ToString();
         }
 
         private void OnDialogClosed(object sender, IDialogClosedEventArgs e)
